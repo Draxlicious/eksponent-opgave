e54d44c049cb0a85b6caa310135849a0cbf4598e
Lukas Mark Jakobsen <lukasjakobsen17@hotmail.com>
2022-03-27 23:42:39 +0200

    setting up vue project with call from API request

---
+++ eksponent-api/src/components/ApiPage.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+       {{ text }}
+        <!-- <div v-for="item in list" v-bind:key="item.id">
+            </div>
+                <p>{{item.employee_name}}<p>
+                <p>{{item.employee_employee_salary}}<p>
+                <p>{{item.employee_age}}<p>
+                <p>{{item.id}}<p>
+            </div>
+        </div> -->
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            text: 'Welcome to eksponent'
+        }
+    },
+    created(){
+        // fetch("http://dummy.restapiexample.com/api/v1/employees")
+        fetch("https://shrouded-peak-36914.herokuapp.com/")
+        .then( response => {
+            return response.json();
+        })
+        .then( data => {
+            this.list = data
+            // console.log(data)
+            console.log(data[0])
+        })
+        // .catch(err => console.log(err))
+    }
+}
+
+</script>
