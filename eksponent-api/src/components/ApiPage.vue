213e9728528f63eab0db8893443704f8bbc6aab8
Lukas Mark Jakobsen <lukasjakobsen17@hotmail.com>
2022-03-28 01:59:56 +0200

    get api images to display

--- eksponent-api/src/components/ApiPage.vue
+++ eksponent-api/src/components/ApiPage.vue
@@ -1,14 +1,15 @@
 <template>
     <div>
        {{ text }}
-        <!-- <div v-for="item in list" v-bind:key="item.id">
+        <div v-for="item in list" v-bind:key="item.id">
+            <div>
+                <p>{{item.id}}</p>
+                <p>{{item.navn}}</p>
+                <p>{{item.telefonnummer}}</p>
+                <p>{{item.motto}}</p>
+                <img :src="item.imageUrl" />
             </div>
-                <p>{{item.employee_name}}<p>
-                <p>{{item.employee_employee_salary}}<p>
-                <p>{{item.employee_age}}<p>
-                <p>{{item.id}}<p>
-            </div>
-        </div> -->
+        </div>
     </div>
 </template>
 
@@ -21,14 +22,14 @@ export default {
     },
     created(){
         // fetch("http://dummy.restapiexample.com/api/v1/employees")
-        fetch("https://shrouded-peak-36914.herokuapp.com/")
+        fetch("https://shrouded-peak-36914.herokuapp.com/persons")
         .then( response => {
             return response.json();
         })
         .then( data => {
             this.list = data
-            // console.log(data)
-            console.log(data[0])
+            console.log(data)
+            // console.log(data[0])
         })
         // .catch(err => console.log(err))
     }
